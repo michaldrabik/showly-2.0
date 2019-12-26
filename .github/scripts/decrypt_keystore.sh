1055772271bd08bdf590b302a4cf2868e769b9b2
Mike <michal.drabik0@gmail.com>
2019-12-27 00:44:36 +0100

    Update decrypt_keystore.sh

--- .github/scripts/decrypt_keystore.sh
+++ .github/scripts/decrypt_keystore.sh
@@ -3,5 +3,6 @@
 # Decrypt the file
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
+pwd
 gpg --quiet --batch --yes --decrypt --passphrase="$KEYSTORE_PASSPHRASE" \
 --output $HOME/secrets/keystore ../keystore.gpg
