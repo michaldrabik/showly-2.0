6bc08df782329fe0e21d67cc108d2b650027f9ca
Michal <michal.drabik0@gmail.com>
2019-12-27 00:24:36 +0100

    Add decrypt file

---
+++ app/decrypt_keystore.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$KEYSTORE_PASSPHRASE" \
+--output $HOME/secrets/keystore keystore.gpg
