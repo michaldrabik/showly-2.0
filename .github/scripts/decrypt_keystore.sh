6d2b0e6b6b7a4b518967c8fdb6cea8092ebd6ba9
Michal <michal.drabik0@gmail.com>
2019-12-27 01:00:11 +0100

    Chore

--- .github/scripts/decrypt_keystore.sh
+++ .github/scripts/decrypt_keystore.sh
@@ -5,3 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$KEYSTORE_PASSPHRASE" --output ./app/keystore ./.github/keystore.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$KEYSTORE_PASSPHRASE" --output ./app/keystore.properties ./.github/keystore.properties.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$KEYSTORE_PASSPHRASE" --output ./local.properties ./.github/local.properties.gpg
