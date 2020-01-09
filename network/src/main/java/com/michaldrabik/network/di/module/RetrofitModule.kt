439e78484828f6da97af12df59b99c607f8cee9e
Michal <michal.drabik@neoncu.be>
2020-01-09 13:49:50 +0100

    Increase timeout duration

--- network/src/main/java/com/michaldrabik/network/di/module/RetrofitModule.kt
+++ network/src/main/java/com/michaldrabik/network/di/module/RetrofitModule.kt
@@ -23,6 +23,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Named
 
 @Module
@@ -93,6 +94,9 @@ object RetrofitModule {
     traktInterceptor: TraktInterceptor
   ): OkHttpClient =
     OkHttpClient.Builder()
+      .readTimeout(20, TimeUnit.SECONDS)
+      .writeTimeout(20, TimeUnit.SECONDS)
+      .connectTimeout(20, TimeUnit.SECONDS)
       .addInterceptor(httpLoggingInterceptor)
       .addInterceptor(traktInterceptor)
       .build()
