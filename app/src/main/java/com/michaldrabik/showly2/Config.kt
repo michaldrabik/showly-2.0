5581620791f2efdea60589149187d011b54b8c14
Michal <michal.drabik@neoncu.be>
2020-01-10 11:19:04 +0100

    Chore Config changes

--- app/src/main/java/com/michaldrabik/showly2/Config.kt
+++ app/src/main/java/com/michaldrabik/showly2/Config.kt
@@ -12,17 +12,17 @@ object Config {
   const val PULL_TO_REFRESH_COOLDOWN_MS = 15_000
   const val SEARCH_RECENTS_AMOUNT = 5
   const val IMAGE_FADE_DURATION_MS = 200
-  const val MY_SHOWS_RECENTS_DEFAULT = 6
   const val FANART_GALLERY_IMAGES_LIMIT = 30
+  const val MY_SHOWS_RECENTS_DEFAULT = 6
   val MY_SHOWS_RECENTS_OPTIONS = arrayOf(2, 4, 6, 8)
 
   val SHOW_DETAILS_CACHE_DURATION by lazy { TimeUnit.DAYS.toMillis(3) }
-  val DISCOVER_SHOWS_CACHE_DURATION by lazy { TimeUnit.HOURS.toMillis(10) }
-  val ACTORS_CACHE_DURATION by lazy { TimeUnit.DAYS.toMillis(7) }
+  val DISCOVER_SHOWS_CACHE_DURATION by lazy { TimeUnit.HOURS.toMillis(12) }
+  val ACTORS_CACHE_DURATION by lazy { TimeUnit.DAYS.toMillis(3) }
   val RELATED_CACHE_DURATION by lazy { TimeUnit.DAYS.toMillis(7) }
   val NEW_BADGE_DURATION by lazy { TimeUnit.HOURS.toMillis(30) }
   val SHOW_SYNC_COOLDOWN by lazy {
-    if (BuildConfig.DEBUG) TimeUnit.MINUTES.toMillis(1) else TimeUnit.HOURS.toMillis(12)
+    if (BuildConfig.DEBUG) TimeUnit.MINUTES.toMillis(5) else TimeUnit.HOURS.toMillis(12)
   }
   val DISPLAY_DATE_FORMAT: DateTimeFormatter by lazy { DateTimeFormatter.ofPattern("EEEE, dd MMM yyyy 'at' HH:mm") }
   val DISPLAY_DATE_DAY_ONLY_FORMAT: DateTimeFormatter by lazy { DateTimeFormatter.ofPattern("dd MMM yyyy") }
