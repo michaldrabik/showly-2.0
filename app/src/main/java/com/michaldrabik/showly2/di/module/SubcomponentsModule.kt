4e4504e05a4f32d7ef468cdd936e2c41342601ba
Michal <michal.drabik0@gmail.com>
2020-02-21 11:34:14 +0530

    Chore Update dependencies

--- app/src/main/java/com/michaldrabik/showly2/di/module/SubcomponentsModule.kt
+++ app/src/main/java/com/michaldrabik/showly2/di/module/SubcomponentsModule.kt
@@ -1,7 +1,8 @@
 package com.michaldrabik.showly2.di.module
 
 import com.michaldrabik.showly2.di.component.FragmentComponent
+import com.michaldrabik.showly2.di.component.ServiceComponent
 import dagger.Module
 
-@Module(subcomponents = [FragmentComponent::class])
+@Module(subcomponents = [FragmentComponent::class, ServiceComponent::class])
 class SubcomponentsModule
