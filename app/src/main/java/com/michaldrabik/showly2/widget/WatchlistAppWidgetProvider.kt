d6cd532be6bf57bec670ead4a6357887ef29c7c8
Michal <michal.drabik0@gmail.com>
2020-03-08 13:43:56 +0100

    Setup basic widget files

---
+++ app/src/main/java/com/michaldrabik/showly2/widget/WatchlistAppWidgetProvider.kt
@@ -0,0 +1,16 @@
+package com.michaldrabik.showly2.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class WatchlistAppWidgetProvider : AppWidgetProvider() {
+
+  override fun onUpdate(
+    context: Context?,
+    appWidgetManager: AppWidgetManager?,
+    appWidgetIds: IntArray?
+  ) {
+    super.onUpdate(context, appWidgetManager, appWidgetIds)
+  }
+}
