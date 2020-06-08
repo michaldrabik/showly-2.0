cc3142878dc897027b2d6901bccb0ee4ad4fad80
Michal <michal.drabik0@gmail.com>
2020-06-08 10:25:51 +0200

    Chore Watchlist button dimensions

--- app/src/main/java/com/michaldrabik/showly2/ui/watchlist/views/WatchlistItemView.kt
+++ app/src/main/java/com/michaldrabik/showly2/ui/watchlist/views/WatchlistItemView.kt
@@ -6,6 +6,7 @@ import android.util.AttributeSet
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.michaldrabik.showly2.R
@@ -14,6 +15,7 @@ import com.michaldrabik.showly2.ui.watchlist.recycler.WatchlistItem
 import com.michaldrabik.showly2.utilities.DurationPrinter
 import com.michaldrabik.showly2.utilities.extensions.addRipple
 import com.michaldrabik.showly2.utilities.extensions.bump
+import com.michaldrabik.showly2.utilities.extensions.dimenToPx
 import com.michaldrabik.showly2.utilities.extensions.expandTouch
 import com.michaldrabik.showly2.utilities.extensions.gone
 import com.michaldrabik.showly2.utilities.extensions.onClick
@@ -39,6 +41,8 @@ class WatchlistItemView : ShowView<WatchlistItem> {
   override val placeholderView: ImageView = watchlistItemPlaceholder
 
   private val durationPrinter by lazy { DurationPrinter(context.applicationContext) }
+  private val checkButtonWidth by lazy { context.dimenToPx(R.dimen.watchlistItemCheckButtonWidth) }
+  private val checkButtonHeight by lazy { context.dimenToPx(R.dimen.watchlistItemCheckButtonHeight) }
 
   fun bind(
     item: WatchlistItem,
@@ -82,15 +86,21 @@ class WatchlistItemView : ShowView<WatchlistItem> {
     val hasAired = item.episode.hasAired(item.season)
     val color = if (hasAired) R.color.colorWatchlistEnabledButton else R.color.colorWatchlistDisabledButton
     if (hasAired) {
-      watchlistItemCheckButton.text = ""
-      watchlistItemCheckButton.setIconResource(R.drawable.ic_check)
-      watchlistItemCheckButton.onClick { it.bump { checkClickListener(item) } }
       watchlistItemInfoButton.visible()
+      watchlistItemCheckButton.run {
+        layoutParams = LinearLayout.LayoutParams(checkButtonWidth, checkButtonHeight)
+        text = ""
+        setIconResource(R.drawable.ic_check)
+        onClick { it.bump { checkClickListener(item) } }
+      }
     } else {
-      watchlistItemCheckButton.text = durationPrinter.print(item.episode.firstAired)
-      watchlistItemCheckButton.icon = null
-      watchlistItemCheckButton.onClick { detailsClickListener(item) }
       watchlistItemInfoButton.gone()
+      watchlistItemCheckButton.run {
+        layoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, checkButtonHeight)
+        text = durationPrinter.print(item.episode.firstAired)
+        icon = null
+        onClick { detailsClickListener(item) }
+      }
     }
     watchlistItemCheckButton.setTextColor(ContextCompat.getColor(context, color))
     watchlistItemCheckButton.setStrokeColorResource(color)
