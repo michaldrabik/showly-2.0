d49a752db19f8e701d46363f24c7154d58f31999
Michal <michal.drabik0@gmail.com>
2020-07-15 12:51:01 +0200

    UI changes

--- app/src/main/java/com/michaldrabik/showly2/ui/followedshows/statistics/views/mostWatched/StatisticsMostWatchedShowsView.kt
+++ app/src/main/java/com/michaldrabik/showly2/ui/followedshows/statistics/views/mostWatched/StatisticsMostWatchedShowsView.kt
@@ -4,13 +4,12 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
-import androidx.core.content.ContextCompat
-import com.google.android.material.card.MaterialCardView
+import android.widget.FrameLayout
 import com.michaldrabik.showly2.R
 import kotlinx.android.synthetic.main.view_statistics_card_most_watched_shows.view.*
 
 @SuppressLint("SetTextI18n")
-class StatisticsMostWatchedShowsView : MaterialCardView {
+class StatisticsMostWatchedShowsView : FrameLayout {
 
   constructor(context: Context) : super(context)
   constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
@@ -19,7 +18,6 @@ class StatisticsMostWatchedShowsView : MaterialCardView {
   init {
     inflate(context, R.layout.view_statistics_card_most_watched_shows, this)
     layoutParams = LayoutParams(MATCH_PARENT, MATCH_PARENT)
-    setCardBackgroundColor(ContextCompat.getColor(context, R.color.colorStatisticsCardBackground))
   }
 
   fun bind(items: List<StatisticsMostWatchedItem>) {
