fbfebb6dd09dac8b3babe361a79c880a7305650d
Michal <michal.drabik0@gmail.com>
2020-07-20 18:58:30 +0200

    Support translucent status bar in fanart gallery

--- app/src/main/java/com/michaldrabik/showly2/ui/show/gallery/FanartGalleryFragment.kt
+++ app/src/main/java/com/michaldrabik/showly2/ui/show/gallery/FanartGalleryFragment.kt
@@ -5,7 +5,9 @@ import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_FULL_USER
 import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import androidx.activity.addCallback
+import androidx.core.view.updateMargins
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -14,6 +16,7 @@ import com.michaldrabik.showly2.fragmentComponent
 import com.michaldrabik.showly2.model.IdTrakt
 import com.michaldrabik.showly2.ui.common.base.BaseFragment
 import com.michaldrabik.showly2.ui.show.gallery.recycler.FanartGalleryAdapter
+import com.michaldrabik.showly2.utilities.extensions.doOnApplyWindowInsets
 import com.michaldrabik.showly2.utilities.extensions.nextPage
 import com.michaldrabik.showly2.utilities.extensions.onClick
 import kotlinx.android.synthetic.main.fragment_fanart_gallery.*
@@ -39,6 +42,8 @@ class FanartGalleryFragment : BaseFragment<FanartGalleryViewModel>(R.layout.frag
     super.onViewCreated(view, savedInstanceState)
     requireActivity().requestedOrientation = SCREEN_ORIENTATION_FULL_USER
     setupView()
+    setupStatusBar()
+
     viewModel.run {
       uiLiveData.observe(viewLifecycleOwner, Observer { render(it!!) })
       loadImage(showId)
@@ -63,6 +68,12 @@ class FanartGalleryFragment : BaseFragment<FanartGalleryViewModel>(R.layout.frag
       offscreenPageLimit = 2
       fanartGalleryPagerIndicator.setViewPager(this)
       adapter?.registerAdapterDataObserver(fanartGalleryPagerIndicator.adapterDataObserver)
+    }
+  }
+
+  private fun setupStatusBar() {
+    fanartGalleryBackArrow.doOnApplyWindowInsets { view, insets, _, _ ->
+      (view.layoutParams as ViewGroup.MarginLayoutParams).updateMargins(top = insets.systemWindowInsetTop)
     }
   }
 
