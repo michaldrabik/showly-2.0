d5c974d0d71955b78101f10de23c9b47ee44c21f
michaldrabik <michal.drabik0@gmail.com>
2020-10-27 23:24:44 +0100

    Add 429 header warning interceptor

--- network/src/main/java/com/michaldrabik/network/trakt/TraktInterceptor.kt
+++ network/src/main/java/com/michaldrabik/network/trakt/TraktInterceptor.kt
@@ -1,8 +1,10 @@
 package com.michaldrabik.network.trakt
 
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.michaldrabik.network.Config
 import okhttp3.Interceptor
 import okhttp3.Response
+import timber.log.Timber
 
 class TraktInterceptor : Interceptor {
   override fun intercept(chain: Interceptor.Chain): Response {
@@ -11,6 +13,14 @@ class TraktInterceptor : Interceptor {
       .header("trakt-api-key", Config.TRAKT_CLIENT_ID)
       .header("trakt-api-version", Config.TRAKT_VERSION)
       .build()
-    return chain.proceed(request)
+
+    val response = chain.proceed(request)
+    if (response.code == 429) {
+      //Log Firebase error in case of rate limit hits start appearing.
+      Timber.e("429 Too Many Requests")
+      FirebaseCrashlytics.getInstance().recordException(Throwable("429 Too Many Requests"))
+    }
+
+    return response
   }
 }
