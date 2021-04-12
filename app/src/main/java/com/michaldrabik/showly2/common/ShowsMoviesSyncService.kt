33da46af1625e5c0ac067ab841e953e08c5c0350
michaldrabik <michal.drabik0@gmail.com>
2021-04-12 23:58:39 +0200

    ShowsMoviesSyncService optimization

--- app/src/main/java/com/michaldrabik/showly2/common/ShowsMoviesSyncService.kt
+++ app/src/main/java/com/michaldrabik/showly2/common/ShowsMoviesSyncService.kt
@@ -12,7 +12,9 @@ import com.michaldrabik.ui_base.events.ShowsMoviesSyncComplete
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.runBlocking
 import timber.log.Timber
 import javax.inject.Inject
@@ -27,7 +29,7 @@ class ShowsMoviesSyncService : JobIntentService(), CoroutineScope {
     }
   }
 
-  override val coroutineContext = Job() + Dispatchers.Main
+  override val coroutineContext = Job() + Dispatchers.Default
 
   @Inject lateinit var showsSyncRunner: ShowsSyncRunner
   @Inject lateinit var moviesSyncRunner: MoviesSyncRunner
@@ -35,18 +37,28 @@ class ShowsMoviesSyncService : JobIntentService(), CoroutineScope {
   override fun onHandleWork(intent: Intent) {
     Timber.d("Sync service initialized")
     serviceComponent().inject(this)
-    var syncCount = 0
-    runBlocking {
+
+    val showsAsync = async {
       try {
-        syncCount += showsSyncRunner.run()
+        return@async showsSyncRunner.run()
       } catch (t: Throwable) {
         Logger.record(t, "Source" to "ShowsSyncRunner")
+        return@async 0
       }
+    }
+
+    val moviesAsync = async {
       try {
-        syncCount += moviesSyncRunner.run()
+        return@async moviesSyncRunner.run()
       } catch (t: Throwable) {
         Logger.record(t, "Source" to "MoviesSyncRunner")
+        return@async 0
       }
+    }
+
+    val syncCount = runBlocking {
+      val (count1, count2) = awaitAll(showsAsync, moviesAsync)
+      count1 + count2
     }
 
     if (syncCount > 0) {
@@ -55,7 +67,7 @@ class ShowsMoviesSyncService : JobIntentService(), CoroutineScope {
   }
 
   override fun onDestroy() {
-    coroutineContext.cancelChildren()
+    coroutineContext.cancel()
     Timber.d("Sync service destroyed")
     super.onDestroy()
   }
