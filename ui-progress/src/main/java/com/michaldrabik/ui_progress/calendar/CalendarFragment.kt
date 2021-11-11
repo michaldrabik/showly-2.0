6062eeeaec957245dafb498db28db23f1f7c8360
michaldrabik <michal.drabik0@gmail.com>
2021-11-11 13:19:00 +0100

    Add local search strip for shows and movies progress

--- ui-progress/src/main/java/com/michaldrabik/ui_progress/calendar/CalendarFragment.kt
+++ ui-progress/src/main/java/com/michaldrabik/ui_progress/calendar/CalendarFragment.kt
@@ -15,7 +15,6 @@ import androidx.recyclerview.widget.SimpleItemAnimator
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.michaldrabik.ui_base.BaseFragment
 import com.michaldrabik.ui_base.common.OnScrollResetListener
-import com.michaldrabik.ui_base.common.OnSearchClickListener
 import com.michaldrabik.ui_base.common.views.RateView
 import com.michaldrabik.ui_base.utilities.extensions.dimenToPx
 import com.michaldrabik.ui_base.utilities.extensions.doOnApplyWindowInsets
@@ -31,7 +30,6 @@ import com.michaldrabik.ui_progress.main.ProgressMainFragment
 import com.michaldrabik.ui_progress.main.ProgressMainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_calendar.*
-import kotlinx.android.synthetic.main.fragment_progress.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
