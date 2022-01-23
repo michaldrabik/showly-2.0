c8504f677f4410d44bc33426d59ee9f4089d57eb
michaldrabik <michal.drabik0@gmail.com>
2022-01-23 21:31:40 +0100

    RTL rotation fix

--- ui-progress/src/main/java/com/michaldrabik/ui_progress/progress/views/ProgressHeaderView.kt
+++ ui-progress/src/main/java/com/michaldrabik/ui_progress/progress/views/ProgressHeaderView.kt
@@ -2,13 +2,16 @@ package com.michaldrabik.ui_progress.progress.views
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.text.TextUtils
 import android.util.AttributeSet
+import android.view.View
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.LinearLayout
 import com.michaldrabik.ui_base.utilities.extensions.onClick
 import com.michaldrabik.ui_progress.R
 import com.michaldrabik.ui_progress.progress.recycler.ProgressListItem
 import kotlinx.android.synthetic.main.view_progress_header.view.*
+import java.util.Locale
 
 @SuppressLint("SetTextI18n")
 class ProgressHeaderView : LinearLayout {
@@ -20,6 +23,7 @@ class ProgressHeaderView : LinearLayout {
   var headerClickListener: ((ProgressListItem.Header) -> Unit)? = null
 
   private lateinit var item: ProgressListItem.Header
+  private val isRtl by lazy { TextUtils.getLayoutDirectionFromLocale(Locale.getDefault()) == View.LAYOUT_DIRECTION_RTL }
 
   init {
     inflate(context, R.layout.view_progress_header, this)
@@ -31,6 +35,7 @@ class ProgressHeaderView : LinearLayout {
   fun bind(item: ProgressListItem.Header) {
     this.item = item
     progressHeaderText.setText(item.textResId)
-    progressHeaderArrow.rotation = if (item.isCollapsed) 0F else 90F
+    val rotation = if (isRtl) -90F else 90F
+    progressHeaderArrow.rotation = if (item.isCollapsed) 0F else rotation
   }
 }
