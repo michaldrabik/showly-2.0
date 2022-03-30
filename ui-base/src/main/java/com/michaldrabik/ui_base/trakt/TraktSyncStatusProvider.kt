1e0c747170d7956edb6bfef4e081fb618eeab0ed
Mike <michal.drabik0@gmail.com>
2022-03-30 11:53:00 +0100

    Chore

--- ui-base/src/main/java/com/michaldrabik/ui_base/trakt/TraktSyncStatusProvider.kt
+++ ui-base/src/main/java/com/michaldrabik/ui_base/trakt/TraktSyncStatusProvider.kt
@@ -2,6 +2,7 @@ package com.michaldrabik.ui_base.trakt
 
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -13,7 +14,7 @@ class TraktSyncStatusProvider @Inject constructor() {
   val status = _status.asStateFlow()
 
   fun setSyncing(value: Boolean) {
-    _status.value = value
+    _status.update { value }
     Timber.d("Syncing status changed: $value")
   }
 }
