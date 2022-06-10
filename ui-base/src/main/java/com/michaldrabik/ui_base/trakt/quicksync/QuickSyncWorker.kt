7c20188b0041d1effc48e925d434efe8328643c3
Mike <michal.drabik0@gmail.com>
2022-06-10 10:44:18 +0200

    Move QuickSync service to worker

--- ui-base/src/main/java/com/michaldrabik/ui_base/trakt/quicksync/QuickSyncWorker.kt
+++ ui-base/src/main/java/com/michaldrabik/ui_base/trakt/quicksync/QuickSyncWorker.kt
@@ -1,21 +1,49 @@
 package com.michaldrabik.ui_base.trakt.quicksync
 
 import android.content.Context
-import android.os.Build
+import androidx.hilt.work.HiltWorker
 import androidx.work.Constraints
 import androidx.work.ExistingWorkPolicy.REPLACE
+import androidx.work.ForegroundInfo
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
-import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.michaldrabik.common.errors.ErrorHelper
+import com.michaldrabik.common.errors.ShowlyError.UnauthorizedError
+import com.michaldrabik.repository.UserTraktManager
+import com.michaldrabik.repository.settings.SettingsRepository
+import com.michaldrabik.ui_base.Analytics
+import com.michaldrabik.ui_base.Logger
+import com.michaldrabik.ui_base.R
+import com.michaldrabik.ui_base.events.EventsManager
+import com.michaldrabik.ui_base.events.TraktQuickSyncSuccess
+import com.michaldrabik.ui_base.events.TraktSyncAuthError
+import com.michaldrabik.ui_base.trakt.TraktNotificationWorker
+import com.michaldrabik.ui_base.trakt.quicksync.runners.QuickSyncListsRunner
+import com.michaldrabik.ui_base.trakt.quicksync.runners.QuickSyncRunner
+import com.michaldrabik.ui_base.utilities.extensions.notificationManager
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import timber.log.Timber
 import java.util.concurrent.TimeUnit.SECONDS
 
-class QuickSyncWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+@HiltWorker
+class QuickSyncWorker @AssistedInject constructor(
+  @Assisted context: Context,
+  @Assisted workerParams: WorkerParameters,
+  private val quickSyncRunner: QuickSyncRunner,
+  private val quickSyncListsRunner: QuickSyncListsRunner,
+  private val settingsRepository: SettingsRepository,
+  private val userManager: UserTraktManager,
+  private val eventsManager: EventsManager
+) : TraktNotificationWorker(context, workerParams) {
 
   companion object {
     private const val TAG = "TRAKT_QUICK_SYNC_WORK"
+
+    private const val SYNC_NOTIFICATION_PROGRESS_ID = 916
+    private const val SYNC_NOTIFICATION_ERROR_ID = 917
 
     fun schedule(workManager: WorkManager) {
       val request = OneTimeWorkRequestBuilder<QuickSyncWorker>()
@@ -33,16 +61,70 @@ class QuickSyncWorker(context: Context, workerParams: WorkerParameters) : Worker
     }
   }
 
-  override fun doWork(): Result {
-    QuickSyncService.createIntent(
-      applicationContext
-    ).run {
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        applicationContext.startForegroundService(this)
-      } else {
-        applicationContext.startService(this)
-      }
+  override suspend fun getForegroundInfo(): ForegroundInfo {
+    val theme = settingsRepository.theme
+    return createProgressNotification(theme, null, 0, 0, true)
+  }
+
+  override suspend fun doWork(): Result {
+    Timber.d("Initialized.")
+    if (quickSyncRunner.isRunning || quickSyncListsRunner.isRunning) {
+      Timber.d("Already running. Finishing...")
+      return Result.failure()
     }
+
+    setForegroundProgress()
+
+    try {
+      var count = quickSyncRunner.run()
+      count += quickSyncListsRunner.run()
+      if (count > 0) {
+        eventsManager.sendEvent(TraktQuickSyncSuccess(count))
+        Analytics.logTraktQuickSyncSuccess(count)
+      }
+    } catch (error: Throwable) {
+      handleError(error)
+    } finally {
+      clearRunners()
+      Timber.d("Quick Sync completed.")
+    }
+
     return Result.success()
+  }
+
+  private suspend fun setForegroundProgress() {
+    try {
+      val theme = settingsRepository.theme
+      setForeground(createProgressNotification(theme, null, 0, 0, true))
+    } catch (error: IllegalStateException) {
+      Timber.w(error)
+    }
+  }
+
+  private suspend fun handleError(error: Throwable) {
+    val showlyError = ErrorHelper.parse(error)
+    if (showlyError is UnauthorizedError) {
+      eventsManager.sendEvent(TraktSyncAuthError)
+      userManager.revokeToken()
+    }
+    val notificationMessage = when (showlyError) {
+      is UnauthorizedError -> R.string.errorTraktAuthorization
+      else -> R.string.textTraktQuickSyncErrorFull
+    }
+    applicationContext.notificationManager().notify(
+      SYNC_NOTIFICATION_ERROR_ID,
+      createErrorNotification(R.string.textTraktQuickSyncError, notificationMessage)
+    )
+    Logger.record(error, "Source" to "${QuickSyncWorker::class.simpleName}")
+  }
+
+  private fun clearRunners() {
+    arrayOf(
+      quickSyncRunner,
+      quickSyncListsRunner,
+    ).forEach {
+      it.progressListener = null
+      it.isRunning = false
+    }
   }
 }
