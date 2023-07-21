cea1ca77a6606c562dfb52d2d307476649a90167
michaldrabik <michal.drabik0@gmail.com>
2023-07-21 08:55:19 +0200

    Lint

--- ui-premium/src/main/java/com/michaldrabik/ui_premium/views/PurchaseItemView.kt
+++ ui-premium/src/main/java/com/michaldrabik/ui_premium/views/PurchaseItemView.kt
@@ -58,8 +58,8 @@ class PurchaseItemView : MaterialCardView {
       }
       viewPurchaseItemDescriptionDetails.text =
         "You will be automatically enrolled in a paid subscription at the end of the free period. " +
-          "Cancel anytime during free period if you do not want to convert to a paid subscription. " +
-          "Subscription will be automatically renewed and charged every $period."
+        "Cancel anytime during free period if you do not want to convert to a paid subscription. " +
+        "Subscription will be automatically renewed and charged every $period."
       viewPurchaseItemPrice.text = "${item.price} / $period"
     }
   }
