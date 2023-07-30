91bcc54ec6648a3ef9b2ed7c2320c1773f77126c
michaldrabik <michal.drabik0@gmail.com>
2023-07-30 08:46:42 +0200

    Update FragmentViewBindingDelegate

--- ui-base/src/main/java/com/michaldrabik/ui_base/utilities/FragmentViewBindingDelegate.kt
+++ ui-base/src/main/java/com/michaldrabik/ui_base/utilities/FragmentViewBindingDelegate.kt
@@ -1,61 +1,63 @@
+/*
+ * Copyright 2021 Gabor Varadi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.michaldrabik.ui_base.utilities
 
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-/**
- * Credits to @zhuiden
- * https://gist.github.com/Zhuinden/ea3189198938bd16c03db628e084a4fa?ts=2
- */
 class FragmentViewBindingDelegate<T : ViewBinding>(
   val fragment: Fragment,
-  val viewBindingFactory: (View) -> T
+  val viewBindingFactory: (View) -> T,
 ) : ReadOnlyProperty<Fragment, T> {
 
   private var binding: T? = null
 
   init {
     fragment.lifecycle.addObserver(object : DefaultLifecycleObserver {
-      val viewLifecycleOwnerLiveDataObserver =
-        Observer<LifecycleOwner?> {
-          val viewLifecycleOwner = it ?: return@Observer
-
-          viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
-            override fun onDestroy(owner: LifecycleOwner) {
-              binding = null
-            }
-          })
+      val viewLifecycleOwnerObserver = Observer<LifecycleOwner?> { owner ->
+        if (owner == null) {
+          binding = null
         }
+      }
 
       override fun onCreate(owner: LifecycleOwner) {
-        fragment.viewLifecycleOwnerLiveData.observeForever(viewLifecycleOwnerLiveDataObserver)
+        fragment.viewLifecycleOwnerLiveData.observeForever(viewLifecycleOwnerObserver)
       }
 
       override fun onDestroy(owner: LifecycleOwner) {
-        fragment.viewLifecycleOwnerLiveData.removeObserver(viewLifecycleOwnerLiveDataObserver)
+        fragment.viewLifecycleOwnerLiveData.removeObserver(viewLifecycleOwnerObserver)
       }
     })
   }
 
   override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
     val binding = binding
-    if (binding != null) {
+
+    if (binding != null && binding.root === thisRef.view) {
       return binding
     }
 
-    val lifecycle = fragment.viewLifecycleOwner.lifecycle
-    if (!lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED)) {
-      throw IllegalStateException("Should not attempt to get bindings when Fragment views are destroyed.")
-    }
-
-    return viewBindingFactory(thisRef.requireView()).also { this.binding = it }
+    val view = thisRef.view ?: throw IllegalStateException("Should not attempt to get bindings when the Fragment's view is null.")
+    return viewBindingFactory(view).also { this.binding = it }
   }
 }
 
