5617d2a4dc110fd8ac194776361675ef40664e99
michaldrabik <michal.drabik0@gmail.com>
2023-08-05 19:41:04 +0200

    Remove android-extension for movie discover module

--- ui-discover/src/main/java/com/michaldrabik/ui_discover/views/ShowFanartView.kt
+++ ui-discover/src/main/java/com/michaldrabik/ui_discover/views/ShowFanartView.kt
@@ -2,6 +2,7 @@ package com.michaldrabik.ui_discover.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.michaldrabik.ui_base.common.views.ShowView
@@ -10,10 +11,10 @@ import com.michaldrabik.ui_base.utilities.extensions.onClick
 import com.michaldrabik.ui_base.utilities.extensions.onLongClick
 import com.michaldrabik.ui_base.utilities.extensions.visibleIf
 import com.michaldrabik.ui_discover.R
+import com.michaldrabik.ui_discover.databinding.ViewShowFanartBinding
 import com.michaldrabik.ui_discover.recycler.DiscoverListItem
 import com.michaldrabik.ui_model.ImageStatus.AVAILABLE
 import com.michaldrabik.ui_model.ImageStatus.UNAVAILABLE
-import kotlinx.android.synthetic.main.view_show_fanart.view.*
 
 class ShowFanartView : ShowView<DiscoverListItem> {
 
@@ -21,14 +22,17 @@ class ShowFanartView : ShowView<DiscoverListItem> {
   constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
   constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
+  private val binding = ViewShowFanartBinding.inflate(LayoutInflater.from(context), this)
+
   init {
-    inflate(context, R.layout.view_show_fanart, this)
-    showFanartRoot.onClick { itemClickListener?.invoke(item) }
-    showFanartRoot.onLongClick { itemLongClickListener?.invoke(item) }
+    with(binding) {
+      showFanartRoot.onClick { itemClickListener?.invoke(item) }
+      showFanartRoot.onLongClick { itemLongClickListener?.invoke(item) }
+    }
   }
 
-  override val imageView: ImageView = showFanartImage
-  override val placeholderView: ImageView = showFanartPlaceholder
+  override val imageView: ImageView = binding.showFanartImage
+  override val placeholderView: ImageView = binding.showFanartPlaceholder
 
   private lateinit var item: DiscoverListItem
 
@@ -36,35 +40,39 @@ class ShowFanartView : ShowView<DiscoverListItem> {
     super.bind(item)
     clear()
     this.item = item
-    showFanartTitle.text =
-      if (item.translation?.title.isNullOrBlank()) item.show.title
-      else item.translation?.title
-    showFanartProgress.visibleIf(item.isLoading)
-    showFanartBadge.visibleIf(item.isFollowed)
-    showFanartBadgeLater.visibleIf(item.isWatchlist)
+    with(binding) {
+      showFanartTitle.text =
+        if (item.translation?.title.isNullOrBlank()) item.show.title
+        else item.translation?.title
+      showFanartProgress.visibleIf(item.isLoading)
+      showFanartBadge.visibleIf(item.isFollowed)
+      showFanartBadgeLater.visibleIf(item.isWatchlist)
+    }
     loadImage(item)
   }
 
   override fun loadImage(item: DiscoverListItem) {
     super.loadImage(item)
     if (item.image.status == UNAVAILABLE) {
-      showFanartRoot.setBackgroundResource(R.drawable.bg_media_view_placeholder)
+      binding.showFanartRoot.setBackgroundResource(R.drawable.bg_media_view_placeholder)
     }
   }
 
   override fun onImageLoadFail(item: DiscoverListItem) {
     super.onImageLoadFail(item)
     if (item.image.status == AVAILABLE) {
-      showFanartRoot.setBackgroundResource(R.drawable.bg_media_view_placeholder)
+      binding.showFanartRoot.setBackgroundResource(R.drawable.bg_media_view_placeholder)
     }
   }
 
   private fun clear() {
-    showFanartTitle.text = ""
-    showFanartProgress.gone()
-    showFanartPlaceholder.gone()
-    showFanartRoot.setBackgroundResource(R.drawable.bg_media_view_elevation)
-    showFanartBadge.gone()
-    Glide.with(this).clear(showFanartImage)
+    with(binding) {
+      showFanartTitle.text = ""
+      showFanartProgress.gone()
+      showFanartPlaceholder.gone()
+      showFanartRoot.setBackgroundResource(R.drawable.bg_media_view_elevation)
+      showFanartBadge.gone()
+      Glide.with(this@ShowFanartView).clear(showFanartImage)
+    }
   }
 }
--- ui-discover/src/main/java/com/michaldrabik/ui_discover/views/ShowPremiumView.kt
+++ ui-discover/src/main/java/com/michaldrabik/ui_discover/views/ShowPremiumView.kt
@@ -2,12 +2,12 @@ package com.michaldrabik.ui_discover.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.ImageView
 import com.michaldrabik.ui_base.common.views.ShowView
 import com.michaldrabik.ui_base.utilities.extensions.onClick
-import com.michaldrabik.ui_discover.R
+import com.michaldrabik.ui_discover.databinding.ViewShowPremiumBinding
 import com.michaldrabik.ui_discover.recycler.DiscoverListItem
-import kotlinx.android.synthetic.main.view_show_premium.view.*
 
 class ShowPremiumView : ShowView<DiscoverListItem> {
 
@@ -15,13 +15,14 @@ class ShowPremiumView : ShowView<DiscoverListItem> {
   constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
   constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
+  private val binding = ViewShowPremiumBinding.inflate(LayoutInflater.from(context), this, true)
+
   init {
-    inflate(context, R.layout.view_show_premium, this)
-    viewShowPremiumRoot.onClick { itemClickListener?.invoke(item) }
+    binding.viewShowPremiumRoot.onClick { itemClickListener?.invoke(item) }
   }
 
-  override val imageView: ImageView = viewShowPremiumImageStub
-  override val placeholderView: ImageView = viewShowPremiumImageStub
+  override val imageView: ImageView = binding.viewShowPremiumImageStub
+  override val placeholderView: ImageView = binding.viewShowPremiumImageStub
 
   private lateinit var item: DiscoverListItem
 
