9244b289d014f5ce98427e8569943aba3e09bc63
michaldrabik <michal.drabik0@gmail.com>
2024-10-09 11:11:55 +0200

    Update dependencies and Moshi

--- data-remote/src/main/java/com/michaldrabik/data_remote/di/module/RetrofitModule.kt
+++ data-remote/src/main/java/com/michaldrabik/data_remote/di/module/RetrofitModule.kt
@@ -5,6 +5,7 @@ import com.michaldrabik.data_remote.Config.OMDB_BASE_URL
 import com.michaldrabik.data_remote.Config.TMDB_BASE_URL
 import com.michaldrabik.data_remote.Config.TRAKT_BASE_URL
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -91,9 +92,12 @@ object RetrofitModule {
 
   @Provides
   @Singleton
-  fun providesMoshiFactory(moshi: Moshi): MoshiConverterFactory = MoshiConverterFactory.create(moshi)
+  fun providesMoshiFactory(moshi: Moshi): MoshiConverterFactory = MoshiConverterFactory
+    .create(moshi)
 
   @Provides
   @Singleton
-  fun providesMoshi(): Moshi = Moshi.Builder().build()
+  fun providesMoshi(): Moshi = Moshi.Builder()
+    .addLast(KotlinJsonAdapterFactory())
+    .build()
 }
